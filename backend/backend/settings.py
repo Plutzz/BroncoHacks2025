a212b7652fd1eee7b25c752657d7b77f4c402ad7
Vinh Pham <85326677+p-vinh@users.noreply.github.com>
2025-04-18 00:09:21 -0700

    Updated virtual env to python 3.12

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j=7(fe!%@48l*8jptk)_@=0ndhz7^0(d5)$hd7fu^i+xle-2vf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
