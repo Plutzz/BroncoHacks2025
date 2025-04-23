fcd42d0ddd3414fb167d208864e03bbdf9b7cfcc
Vinh Pham <85326677+p-vinh@users.noreply.github.com>
2025-04-23 01:49:57 -0700

    Test

--- frontend/src/AxiosConfig.js
+++ frontend/src/AxiosConfig.js
@@ -1,19 +1,28 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 // FOR BASE URL: USE 'http://localhost:8000'
-const API = import.meta.env.VITE_API_URL || 'http://0.0.0.0:8080' || "http://127.0.0.1:8000";
 const axiosInstance = axios.create({
-	baseURL: API + "/",
-	withCredentials: true,
-  });
-
-  axiosInstance.get("api/accounts/get_csrf_cookie/");
-
-axiosInstance.interceptors.request.use((config) => {
-  const token = Cookies.get("csrftoken");
-  console.log('CSRF Token in Interceptor:', token);
-  if (token) config.headers["X-CSRFToken"] = token;
-  return config;
+  baseURL: 'https://bronco-hacks2025.vercel.app/',
+  withCredentials: true,  // Important to include cookies (sessionid & csrftoken)
+  xsrfCookieName: 'csrftoken',
+  xsrfHeaderName: 'X-CSRFToken'
 });
+
+axiosInstance.interceptors.request.use(
+config => {
+	const csrfToken = Cookies.get('csrftoken');
+	console.log("CSRF Token in Interceptor:", csrfToken);  // Debugging
+	if (csrfToken) 
+	{
+		config.headers['X-CSRFToken'] = csrfToken;  // Add CSRF token to request header
+	}
+	else
+	{
+		console.log('No CSRF token found.');
+	}
+	return config;
+},
+error => Promise.reject(error)
+);
 
 export default axiosInstance;
